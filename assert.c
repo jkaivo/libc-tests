d946eeba71660b68d77f4a2d2bcf3609d472c2c7
Jakob Kaivo <jkk@ung.org>
2019-01-01 09:18:12 -0500

    basic testing of NDEBUG and assert()

---
+++ assert.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "test.h"
+
+void test_assert(void)
+{
+	int n = 0;
+
+	testing_header("assert.h");
+
+#define NDEBUG
+#include <assert.h>
+	testing_comment("Expression with side-effect should be removed by preprocessor");
+	test_void(assert(n = 1));
+	test_int_equals(n, 0);
+
+#undef NDEBUG
+#include <assert.h>
+	testing_comment("Expression with side-effect should execute");
+	test_void(assert(n = 1));
+	test_int_equals(n, 1);
+
+#define NDEBUG
+#include <assert.h>
+	testing_comment("Successful assertion should be removed by preprocessor");
+	test_void(assert(n == 1));
+
+#undef NDEBUG
+#include <assert.h>
+	testing_comment("Successful assertion should execute");
+	test_void(assert(n == 1));
+
+#define NDEBUG
+#include <assert.h>
+	testing_comment("Unsuccessful assertion should be removed by preprocessor");
+	test_void(assert(n == 0));
+
+#undef NDEBUG
+#include <assert.h>
+	testing_comment("Unsuccessful assertion should execute");
+	test_void(assert(n == 0));
+}
