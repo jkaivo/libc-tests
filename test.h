ae243cbbee09a8ea3528cb7cb963b07f9e4e6fe1
Jakob Kaivo <jkk@ung.org>
2019-02-27 20:24:07 -0500

    don't bother with (void)expression

--- test.h
+++ test.h
@@ -6,7 +6,7 @@ void test_int_equals_imp(const char*, int, int);
 #define test_int_equals(expression, expected) test_int_equals_imp(#expression, expression, expected)
 
 void test_void_imp(const char*);
-#define test_void(expression) test_void_imp(#expression); (void)expression
+#define test_void(expression) test_void_imp(#expression)
 
 void test_bool_imp(const char *, int, int);
 #define test_false(expression) test_bool_imp(#expression, expression, 0)
