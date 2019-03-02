7ddf768dad85f1f6b5f3abb245c49c00e63b7111
Jakob Kaivo <jkk@ung.org>
2019-03-01 19:47:48 -0500

    don't explicitly check for errors to be defined, the compiler will catch it

--- errno.c
+++ errno.c
@@ -1,27 +1,12 @@
 #include <errno.h>
 #include "test.h"
 
-#ifndef EDOM
-#error EDOM not defined
-#endif
-
-#ifndef ERANGE
-#error ERANGE not defined
-#endif
-
-#if __STDC_VERSION__ >= 199409
-#ifndef EILSEQ
-#error EILSEQ not defined
-#endif
-#endif
-
 void test_errno_h(void)
 {
-	struct lconv *lc;
 	int errno_values[] = {
 		EDOM,
 		ERANGE,
-		#ifdef EILSEQ
+		#if defined __STDC_VERSION__
 		EILSEQ,
 		#endif
 	};
