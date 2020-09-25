6a2d72af1ac550f5050eeac0be25734c206b9512
Jakob Kaivo <jkk@ung.org>
2020-09-25 14:20:55 -0400

    make testing different versions easier

--- config.mk
+++ config.mk
@@ -1,3 +1,8 @@
 CC=ungol-gcc
-CFLAGS=-fno-builtin -g -std=iso9899:199409 -Wall -Wextra
+C_VERSION=c89
+#C_VERSION=iso9899:199409
+#C_VERSION=c99
+#C_VERSION=c11
+#C_VERSION=c17
+CFLAGS=-fno-builtin -g -std=$(C_VERSION) -Wall -Wextra
 LDFLAGS=-lc -lm
